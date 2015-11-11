0ebc25a836c2c2227d028ade2d46500239101583
BrittanyNovak <brittanynicole0106@gmail.com>
2015-11-10 20:36:31 -0600

    adding elements to driving java app

--- Driving.java
+++ Driving.java
@@ -6,17 +6,33 @@ public class Driving{
 	public static void main(String [] args){
 	
 		Scanner in = new Scanner(System.in);
+		Scanner keyboard = new Scanner(System.in);
 		Car myCar = new Car();
-		myCar.turnOn();
+		String make;
+		String model;
 		
+			System.out.println("Starting your vehicle");
+			myCar.turnOn();
+			
 		while( myCar.isRun()){
 			System.out.println("Your car is running");
+			System.out.println("Your vehicle is a " + myCar.getMake() + " " + myCar.getModel() );
 			System.out.println("What do you want to do with your car?");
 			System.out.println("0 - Turn car off");
+			System.out.println("1 - Change Make of Vehicle");
+			System.out.println("2 - Change Model of Vehicle");
 			int answer = in.nextInt();
 			
 			if(answer == 0){
 				myCar.turnOff();
+			} else if(answer == 1){
+				System.out.println("New Make of Vehicle?");
+				make = keyboard.next();
+				myCar.setMake(make);
+			} else if(answer == 2){
+				System.out.println("New Model of Vehicle");
+				model = keyboard.next();
+				myCar.setModel(model);
 			}
 		}
 	}
--- vehicle/Car.java
+++ vehicle/Car.java
@@ -3,6 +3,9 @@ package vehicle;
 public class Car{
 	
 	private boolean isRunning;
+	private String make = "No Make";
+	private String model = "No Model";
+	private int numOfWheels = 4;
 	
 	public void Car(){
 		isRunning = false;
@@ -18,5 +21,21 @@ public class Car{
 	
 	public boolean isRun(){
 		return isRunning;
+	}
+	
+	public String getModel(){
+		return model;
+	}
+	
+	public String getMake(){
+		return make;
+	}
+	
+	public void setMake(String b){
+		make = b;
+	}
+	
+	public void setModel(String m){
+		model = m;
 	}
 }
